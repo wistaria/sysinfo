b34d535f978c1953afff34b0f9d2ff9454fee867
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2026-02-02 17:06:55 +0900

    update

--- sysinfo.sh
+++ sysinfo.sh
@@ -74,25 +74,25 @@ fi
 N_GPUS=0
 MODEL_GPUS=""
 if command -v nvidia-smi >/dev/null 2>&1; then
-  N_GPUS=$(nvidia-smi -L | grep -c 'GPU')
   MODEL_GPUS=$(nvidia-smi -L | grep GPU | head -1 | awk '{  for (i = 3; i <= NF-2; i++) { printf "%s%s", $i, (i < NF-2 ? OFS : ORS) }}')
+  N_GPUS=$(nvidia-smi -L | grep -c 'GPU')
 elif [ "$(uname -s 2>/dev/null)" = "Darwin" ] && command -v system_profiler >/dev/null 2>&1; then
-  N_GPUS="$(system_profiler SPDisplaysDataType | awk -F': ' '/Total Number of Cores/ {print $2}')"
   MODEL_GPUS="$(system_profiler SPDisplaysDataType 2>/dev/null | awk -F': ' '/Chipset Model:/ {print $2; exit}')"
+  N_GPUS="$(system_profiler SPDisplaysDataType | awk -F': ' '/Total Number of Cores/ {print $2}')"
 fi
 
 if [ $FLAG_TABLE -eq 1 ]; then
-  printf "| %s | %s | %s | %d | %d | %d | %d | %s | %d | %d | %s |\n" "${HOSTNAME}" "${OS}" "${MODEL}" "${N_CPUS}" "${N_PHYS_CORES}" "${N_LOG_CORES}" "${HYPER_THREAD}" "${BOGOMIPS}" "${MEMORY}" "${N_GPUS}" "${MODEL_GPUS}"
+  printf "| %s | %s | %s | %d | %d | %d | %d | %s | %d | %d | %s |\n" "${HOSTNAME}" "${OS}" "${MODEL}" "${N_CPUS}" "${N_PHYS_CORES}" "${N_LOG_CORES}" "${HYPER_THREAD}" "${BOGOMIPS}" "${MEMORY}" "${MODEL_GPUS}" "${N_GPUS}" 
 else
   printf "%-32s : %s\n" "Hostname" "${HOSTNAME}"
   printf "%-32s : %s\n" "OS" "${OS}"
   printf "%-32s : %s\n" "Model" "${MODEL}"
-  printf "%-32s : %d\n" "Total Number of CPUs" "${N_CPUS}"
+  printf "%-32s : %d\n" "Total Number of CPU(s)" "${N_CPUS}"
   printf "%-32s : %d\n" "Number of Physical Cores per CPU" "${N_PHYS_CORES}"
   printf "%-32s : %d\n" "Total Number of Logical Cores" "${N_LOG_CORES}"
   printf "%-32s : %d\n" "Hyper Threading" "${HYPER_THREAD}"
   printf "%-32s : %s\n" "BogoMIPS" "${BOGOMIPS}"
   printf "%-32s : %d\n" "Total Memory (GB)" "${MEMORY}"
-  printf "%-32s : %d\n" "Number of GPUs" "${N_GPUS}"
-  printf "%-32s : %s\n" "MODEL of GPUs" "${MODEL_GPUS}"
+  printf "%-32s : %s\n" "MODEL of GPU(s)" "${MODEL_GPUS}"
+  printf "%-32s : %d\n" "Number of GPU(s)" "${N_GPUS}"
 fi
